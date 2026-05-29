46d90010fbb44439c7485b0922fde937af08ec5f
Mr. Aero <155216937+mraero006@users.noreply.github.com>
2026-05-29 02:46:51 -0400

    Update screenshot.h

--- include/engine/screenshot.h
+++ include/engine/screenshot.h
@@ -13,4 +13,6 @@ void Debug_TakeScreenshotBMP(void);
 void D3D_SaveScreen(const char* filename, u16* buffer);
 void D3D_TakeScreenshotBMP(u16* buffer);*/
 
+void Debug_TakeScreenshotBMP(u16* vram1, u16* vram2);
+
 #endif
